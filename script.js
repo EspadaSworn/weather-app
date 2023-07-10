a6a45828258d019ee4b2159995de418e8d4e82e4
brandon <natakumon2@yahoo.com>
2023-07-09 21:37:12 -0700

    working display for current date

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 
 //for current weather
 //var search = document.getElementByClassName('search');
+var citySearched = [];
 var input = document.getElementById('search-bar');
 input.addEventListener('keydown', function (event) {
   if (event.key === "Enter") {
@@ -24,26 +25,69 @@ input.addEventListener('keydown', function (event) {
 
 
 function getData(query) {
-  fetch('https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${511c4376976a27f45a735b245d107187}')
-    .then(function (response) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?appid=511c4376976a27f45a735b245d107187&q=${query}&units=imperial`)
+    .then(function (response) { 
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      //   renderResponse(data);
+      console.log(data.main.temp);
+      if (citySearched.includes(data.name) === false) {
+        citySearched.push(data.name);
+        localStorage.setItem('city', JSON.stringify(citySearched));
+      }
+      
+      renderResponse(data);
+
+                  var weatherSymbol = data.weather[0].icon;
+            var symbolUrl = `https://openweathermap.org/img/wn/` + weatherSymbol + `@2x.png`;
+            var today = dayjs();
+            var city = document.getElementById('currentCity');
+             city.innerHTML = (data.name + '' + ' (' + today.format('MM/DD/YYYY') + ')' + '<img src="' + symbolUrl + '">');
+
+            var temp = document.getElementById('currentTemp');
+            temp.textContent = 'Temperature: ' + data.main.temp + ' °F';
+
+            var wind = document.getElementById('currentWind');
+            wind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
+
+            var humidity = document.getElementById('currentHumidity');
+            humidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
+
+
+
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            console.log('lat: ' + lat + ' lon:' + lon);
+            secondApi(lat, lon);
     });
 
   //for forecast
-  fetch('https://api.openweathermap.org/data/2.5/weather?appid=${511c4376976a27f45a735b245d107187}&q=${city}&units=imperial')
-    .then(function (response) {
+ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=511c4376976a27f45a735b245d107187`)
+  .then(function (response) { //how to replace {city} with value
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      //   renderResponse(data);
-      console.log(input)
-    });
+       console.log(data);
+       //   renderResponse(data);
+       console.log(input)
+     });
 }//}
 //Hint: The forecast data is every 3 hours for 5 days, so you will have a list of 40 forecasts. Your requirement is to give 5 days of forecast data.
 // How can you make a for loop that will "jump over" indexes that you aren't interested in?
 
+function renderResponse() {
+  if (localStorage.getItem('city')){
+    citySearched = JSON.parse(localStorage.getItem('city'));
+  }
+
+  var cityList = document.getElementById('city-list');  //create a list of cities searched
+
+  //  cityList.innerHTML = '';
+
+  for (var i = 0; i < citySearched.length; i++) {
+    var cityHistoryBtn = document.createElement('button');
+    cityHistoryBtn.classList.add('btn', 'btn-secondary', 'btn-block', 'mb-1');
+  }}
+
+  renderResponse();
